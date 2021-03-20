1cf249b6db3d3426f666dbf2877d89352095a80f
Mel <Chosse.Martiny01@gmail.com>
2021-03-20 16:54:43 +0100

    Implementation of Horizontal Tabs
    Implemented horizontal tabs for Output and changed colors of Quantity Amounts

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -2,6 +2,8 @@
 
 Please feel free to help us with the application as much as you like!
 
+We have created a Projects board for the features to implement and think about. It is recommended to check out 💯
+
 🔎 We are always available on discord, so check out the **README** and **SECURITY** files for more info!
 
 - **TIP:** *Make sure to create a fork before you start working on the project!*
--- src/components/CalculatorOutput.js
+++ src/components/CalculatorOutput.js
@@ -1,9 +1,11 @@
-import { Typography, Grid } from "@material-ui/core";
+import { Typography, Grid, Tabs, Tab, Box } from "@material-ui/core";
 import IconWithText from "./IconWithText";
 import ChancesTable from "./ChancesTable";
 import QuantityTable from "./QuantityTable";
 import PaperItem from "./PaperItem";
 import { makeStyles } from "@material-ui/core/styles";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles((theme) => ({
 	pt16: {
@@ -23,51 +25,97 @@ const useStyles = makeStyles((theme) => ({
 	},
 }));
 
+function TabPanel(props) {
+	const { children, value, index, ...other } = props;
+
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`scrollable-force-tabpanel-${index}`}
+			aria-labelledby={`scrollable-force-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box p={3}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
+}
+TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.any.isRequired,
+	value: PropTypes.any.isRequired,
+};
+function a11yProps(index) {
+	return {
+		id: `vertical-tab-${index}`,
+		"aria-controls": `vertical-tabpanel-${index}`,
+	};
+}
+
 const CalculatorOutput = ({ calculatedData }) => {
 	const classes = useStyles();
+	const [value, setValue] = useState(0);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
 
 	return (
 		<PaperItem className={`${classes.p16} ${classes.paper} `}>
-			<Typography variant="h6">
-				Output
-				<Typography variant="caption" className={classes.p16}>
-					Quantity Find % does not effect the Rarity Table
-				</Typography>
-			</Typography>
-			<Typography className={`${classes.pt16} ${classes.p8}`}>
-				<b>Normal monsters slain:</b>{" "}
-				{Math.ceil(calculatedData.monstersSlain.normal)}
-				<br />
-				<b>Bosses slain:</b>{" "}
-				{Math.ceil(calculatedData.monstersSlain.bosses)}
-			</Typography>
-			<ChancesTable
-				data={calculatedData.chancesOutput}
-				className={classes.mt8}
-			/>
-			<QuantityTable
-				data={calculatedData.quantitiesOutput}
-				className={classes.mt8}
-			/>
-			<Grid
-				container
-				justify="space-between"
-				direction="row"
-				className={`${classes.pt16} ${classes.p8}`}
+			<Tabs
+				orientation="horizontal"
+				value={value}
+				onChange={handleChange}
+				aria-label="Vertical tabs example"
+				indicatorColor="primary"
+				centered
 			>
-				<Typography>
-					<b>You will earn:</b>
+				<Tab label="Chances" {...a11yProps(0)} />
+				<Tab label="Quantity Details" {...a11yProps(1)} />
+			</Tabs>
+			<Typography className={`${classes.p8}`}>
+				Normal monsters slain:{" "}
+				<b>{Math.ceil(calculatedData.monstersSlain.normal)}</b>
+				<br />
+				Bosses slain:{" "}
+				<b>{Math.ceil(calculatedData.monstersSlain.bosses)}</b>
+			</Typography>
+			<TabPanel value={value} index={0}>
+				<Typography variant="caption" className={classes.p16}>
+					<i>Quantity Find % does not effect the Rarity Table</i>
 				</Typography>
-				<IconWithText icon="icons/icon_gold.png" tooltip="Gold">
-					Gold: <b>{calculatedData.currencyAmount.gold}</b>
-				</IconWithText>
-				<IconWithText icon="icons/icon_silver.png" tooltip="Silver">
-					Silver: <b>{calculatedData.currencyAmount.silver}</b>
-				</IconWithText>
-				<IconWithText icon="icons/icon_copper.png" tooltip="Copper">
-					Copper: <b>{calculatedData.currencyAmount.copper}</b>
-				</IconWithText>
-			</Grid>
+				<ChancesTable
+					data={calculatedData.chancesOutput}
+					className={classes.mt8}
+				/>
+				<Grid
+					container
+					justify="space-between"
+					direction="row"
+					className={`${classes.p8}`}
+				>
+					<Typography>You will earn:</Typography>
+					<IconWithText icon="icons/icon_gold.png" tooltip="Gold">
+						<b>{calculatedData.currencyAmount.gold}</b>
+					</IconWithText>
+					<IconWithText icon="icons/icon_silver.png" tooltip="Silver">
+						<b>{calculatedData.currencyAmount.silver}</b>
+					</IconWithText>
+					<IconWithText icon="icons/icon_copper.png" tooltip="Copper">
+						<b>{calculatedData.currencyAmount.copper}</b>
+					</IconWithText>
+				</Grid>
+			</TabPanel>
+			<TabPanel value={value} index={1}>
+				<QuantityTable
+					data={calculatedData.quantitiesOutput}
+					className={classes.mt8}
+				/>
+			</TabPanel>
 		</PaperItem>
 	);
 };
